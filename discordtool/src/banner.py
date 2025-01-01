3c1315bdfcd79c752417174890f01c7771603c4c
noisingAgus <131608884+noisingAgus@users.noreply.github.com>
2025-01-01 17:19:14 -0300

    Update banner.py

--- discordtool/src/banner.py
+++ discordtool/src/banner.py
@@ -1,9 +1,9 @@
-# src/banner.py
+
 
 from src.utils.colors import red, grey, light_green, dark_grey, reset
 
 def print_ascii_banner():
-    # Arte ASCII personalizado
+    
     banner = red + """
      ▐▄▄▄ ▄▄▄· ▪  ▄▄▄            ▄▄▄▄▄            ▄▄▌  
   ·██▐█ ▀█ ██ ▀▄ █·▪         •██  ▪     ▪     ██•  
@@ -12,10 +12,10 @@ def print_ascii_banner():
  ▀▀▀• ▀  ▀ ▀▀▀.▀  ▀ ▀█▄▀▪     ▀▀▀  ▀█▄▀▪ ▀█▄▀▪.▀▀▀ 
     """ + reset
 
-    # Texto de descripción en gris oscuro y versión en verde claro
+    
     description = grey + "Advanced tool for pentesting discord servers" + reset
     version = light_green + " [1.0.0]" + reset
 
-    # Imprimir el banner y la descripción en la forma deseada
+    
     print(banner)
-    print(description + version + "\n")  # Concatenamos el texto y la versión en un solo print
+    print(description + version + "\n")  
